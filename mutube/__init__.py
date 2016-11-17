2f856bd732e0ab31018983c960d989c3d670d4d7
AP-e <AP-e@users.noreply.github.com>
2016-11-17 19:20:03 +0000

    Expose exceptions at package level

--- mutube/__init__.py
+++ mutube/__init__.py
@@ -1,3 +1,3 @@
-from .exceptions import NoTag, NoPlaylist
+from .exceptions import NoTag, NoPlaylist, BadVideo
 from .scraper import Scraper
-from .playlister import Playlister, encode_tag, decode_tag
+from .playlister import Playlister, encode_tag, decode_tag, HttpError
