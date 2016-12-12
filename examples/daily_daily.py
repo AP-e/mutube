451cb4023db42bb45379af9dd7f373b27601f0b8
AP-e <AP-e@users.noreply.github.com>
2016-12-12 16:04:27 +0000

    Update examples

--- examples/daily_daily.py
+++ examples/daily_daily.py
@@ -3,17 +3,29 @@
 This script automatically generates daily playlists of YouTube links posted
 to the /daily/ general threads on 4chan's /mu/.
 """
-from mutube import Mutuber
+from mutube import Scraper, Playlister, Mutuber
 
-if __name__ == "__main__":                                                      
-    # Initialise mutuber object                                                 
-    mutuber = Mutuber(                                           
-            board = 'mu',
-            subjects = ['/daily/'],
-            prefix = 'daylist',
-            time_format = '%A %d.%m.%Y', # i.e. "Thursday 23.06.2016"
-            client_json = 'client_id.json',
-            current_only = True, # allow reposting between different playlists
-            playlister_pause = 1, # one insertion per minute
-            scraper_pause = 10) # scrape every 10 minutes
-    mutuber.run_forever() 
+def first_word_matcher(subject, general):
+    """ Determine whether first word in `subject` == `general`.
+    Args:
+        subject ::: (str) thread subject
+        general ::: (str) general name
+    """
+    return True if subject.split('-')[0].strip() == general else False
+
+if __name__ == "__main__":
+    # Initialise objects 
+    scraper = Scraper(board='mu',
+                      matching_func=first_word_matcher, # custom matching 
+                      general="/daily/") # argument to matching function
+    playlister = Playlister(
+                    prefix='DAYLIST',
+                    time_format='%A %d.%m.%Y', # Thursday 23.06.2016
+                    client_json='client_id.json')
+    mutuber = Mutuber(scraper, playlister,
+                      current_only=True) # repost between different playlists
+    
+    # Run continuously 
+    mutuber.run_forever(
+        playlister_pause = 1, # one insertion per minute
+        scraper_pause = 10) # scrape every 10 minutes
--- examples/metal_monthly.py
+++ examples/metal_monthly.py
@@ -3,18 +3,15 @@
 This script automatically generates monthly playlists of YouTube links posted
 to the /metal/ general threads on 4chan's /mu/.
 """
-from mutube import Mutuber
+from mutube import Scraper, Playlister, Mutuber
 
-if __name__ == "__main__":                                                      
+if __name__ == "__main__":
     # Initialise mutuber object                                             
-    mutuber = Mutuber(
-            board = 'mu',
-            subjects = ['/metal/'],
-            prefix = r'\m/',
-            time_format = '%b %Y', # i.e. "Nov 2016" 
-            client_json = 'client_id.json',
-            current_only = False, # ignore all previously posted links
-            playlister_pause = 1, # one insertion per minute
-            scraper_pause = 60) # scrape once per hour
+    scraper = Scraper(board='mu', subjects=['/metal/',
+                                            '/metal/ - Metal General'])
+    playlister = Playlister(prefix = r'\m/',
+                            time_format = '%b %Y') # i.e. "Nov 2016"
+                            
+    mutuber = Mutuber(scraper, playlister)
     mutuber.run_forever()
 
