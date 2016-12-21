fadcdb7cc8b47e7806d72e49ed24147b6a658deb
AP-e <AP-e@users.noreply.github.com>
2016-12-21 00:50:24 +0000

    Update examples

--- examples/daily_daily.py
+++ examples/daily_daily.py
@@ -3,7 +3,7 @@
 This script automatically generates daily playlists of YouTube links posted
 to the /daily/ general threads on 4chan's /mu/.
 """
-from mutube import Scraper, Playlister, Mutuber
+from mutube import Scraper, Playlister, Mutuber, ResourceBuilder
 
 def first_word_matcher(subject, general):
     """ Determine whether first word in `subject` == `general`.
@@ -14,14 +14,13 @@ def first_word_matcher(subject, general):
     return True if subject.split('-')[0].strip() == general else False
 
 if __name__ == "__main__":
-    # Initialise objects 
+    # Initialise objects
     scraper = Scraper(board='mu',
                       matching_func=first_word_matcher, # custom matching 
-                      general="/daily/") # argument to matching function
-    playlister = Playlister(
-                    prefix='DAYLIST',
-                    time_format='%A %d.%m.%Y', # Thursday 23.06.2016
-                    client_json='client_id.json')
+                      general="/daily/") # argument to matching function 
+    resource = ResourceBuilder.from_client_credentials_file('client_id.json') 
+    playlister = Playlister(resource=resource, prefix='DAYLIST',
+                            time_format='%A %d.%m.%Y') # e.g. Thursday 23.06.2016
     mutuber = Mutuber(scraper, playlister,
                       current_only=True) # repost between different playlists
     
--- examples/metal_monthly.py
+++ examples/metal_monthly.py
@@ -3,15 +3,15 @@
 This script automatically generates monthly playlists of YouTube links posted
 to the /metal/ general threads on 4chan's /mu/.
 """
-from mutube import Scraper, Playlister, Mutuber
+from mutube import Scraper, Playlister, Mutuber, ResourceBuilder
 
 if __name__ == "__main__":
     # Initialise mutuber object                                             
     scraper = Scraper(board='mu', subjects=['/metal/',
                                             '/metal/ - Metal General'])
-    playlister = Playlister(prefix = r'\m/',
+    resource = ResourceBuilder.from_user_credentials_file('client_id.json')
+    playlister = Playlister(resource=resource, prefix=r'\m/',
                             time_format = '%b %Y') # i.e. "Nov 2016"
-                            
     mutuber = Mutuber(scraper, playlister)
     mutuber.run_forever()
 
